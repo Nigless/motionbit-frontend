eed60317bf83dcb05d2d53d619f677f186621679
Nigless <nigless>
2020-10-19 17:51:45 +0000

    Добавил файл с константами

---
+++ src/variables.ts
@@ -0,0 +1,3 @@
+﻿export const
+	MAIN_FONT: string = '-apple-system,BlinkMacSystemFont, "Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji", monospace',
+	FONT_SIZE: string = 16 + 'px'

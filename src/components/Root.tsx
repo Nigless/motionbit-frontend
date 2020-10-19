d2abc824d33c1520360f02ec77074207250b1e66
Nigless <nigless.gaming@gmail.com>
2020-10-19 20:46:18 +0300

    Добавил компонент Root

---
+++ src/components/Root.tsx
@@ -0,0 +1,14 @@
+﻿import { cloneElement, createElement, ReactElement, ReactNode } from "react";
+
+interface Props {
+	className?: string,
+	children: ReactNode
+}
+
+export default function Root({ className, children }: Props) {
+
+	return Array.isArray(children)
+		? createElement('div', { className: className }, children)
+		: cloneElement((children as ReactElement), { className: className })
+
+}

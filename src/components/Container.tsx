b63264fd685d2e6b65b039d7c0757c76b9e041ae
Nigless <nigless.gaming@gmail.com>
2020-10-19 20:47:48 +0300

    Добавил компонент Container

---
+++ src/components/Container.tsx
@@ -0,0 +1,25 @@
+﻿import React, { ReactNode } from "react";
+import styled from "styled-components"
+import Root from './Root'
+
+
+interface Props {
+	className?: string
+	children?: ReactNode
+	width?: string
+	maxWidth?: string
+	wrap?: string
+}
+
+export default function Container({ className, children, width, maxWidth, wrap }: Props) {
+	const Wrapper = styled(wrap || Root)({
+		width: width,
+		maxWidth: maxWidth,
+		margin: 'auto'
+	})
+	return (
+		<Wrapper className={className}>
+			{children}
+		</Wrapper>
+	)
+}

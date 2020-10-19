38bd492cc48ce6eaa19471eeea2a154377dff7dd
Nigless <nigless.gaming@gmail.com>
2020-10-19 21:00:37 +0300

    Удалил мусор

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,40 +11,10 @@ const Wrapper = styled(Box)({
 	padding: '.1px 0'
 })
 
-const Xyu = styled.div({
-	border: 'solid #fff 3px'
-})
 export default function Navigation() {
 	return (
 		<Wrapper>
 			<Container maxWidth="1000px" >
-				<Grid>
-					<Item width="50px">
-						<Xyu>lol</Xyu>
-					</Item>
-					<Item width="10fr">
-						<Xyu>lol</Xyu>
-					</Item>
-					<Item width="5fr">
-						<Xyu>lol</Xyu>
-					</Item>
-					<Item width="40px">
-						<Xyu>lol</Xyu>
-					</Item>
-					<Item width="40%">
-						<Xyu>lol</Xyu>
-					</Item>
-					<Break />
-					<Item width="1fr">
-						<Xyu>lol</Xyu>
-					</Item>
-					<Item width="20fr">
-						<Xyu>lol</Xyu>
-					</Item>
-					<Item width="40%">
-						<Xyu>lol</Xyu>
-					</Item>
-				</Grid>
 			</Container>
 		</Wrapper>
 	)

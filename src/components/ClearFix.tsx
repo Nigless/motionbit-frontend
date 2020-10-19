8678524d0ca482281ea880dbb09be0307f98ab31
Nigless <nigless>
2020-10-19 17:48:17 +0000

    Добавил компонент ClearFix

---
+++ src/components/ClearFix.tsx
@@ -0,0 +1,24 @@
+﻿import React, { ReactNode } from "react"
+import styled from "styled-components"
+import Root from './Root'
+
+interface Props {
+	className?: string,
+	wrap?: string,
+	children: ReactNode
+}
+
+export default function ClearFix({ className, wrap, children }: Props) {
+	const Wrapper = styled(wrap || Root)({
+		'&::after': {
+			content: '""',
+			clear: 'both',
+			display: 'table'
+		}
+	})
+	return (
+		<Wrapper className={className}>
+			{children}
+		</Wrapper>
+	)
+}

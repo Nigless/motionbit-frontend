a6a9cebd7493a5a9e18f94a79f99face2e2a0043
Nigless <nigless.gaming@gmail.com>
2020-10-23 17:42:47 +0300

    добавил компонент ссылок для футера

---
+++ src/components/Footer/A.tsx
@@ -0,0 +1,14 @@
+﻿import styled from "styled-components"
+
+const A = styled.a({
+	color: '#888',
+	textDecoration: 'none',
+	fontSize: '0.8em',
+	'&:hover': {
+		color: '#fff'
+	},
+	'&:visited': {
+		color: '#a559a2'
+	}
+})
+export default A

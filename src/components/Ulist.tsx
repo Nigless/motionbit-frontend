234ebd9fc81092c69d82ddfe02c0901930d55d98
Nigless <nigless.gaming@gmail.com>
2020-10-23 17:34:45 +0300

    добавил компонент Ulist

---
+++ src/components/Ulist.tsx
@@ -0,0 +1,12 @@
+﻿import React, { Children, ReactNode } from "react";
+
+interface Props {
+	children: ReactNode
+	className?: string
+}
+
+export default function A({ children, className }: Props) {
+	return <ul className={className}>{
+		Children.map(children, child => <li>{child}</li>)
+	}</ul>
+}

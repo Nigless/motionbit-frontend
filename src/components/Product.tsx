666497b72b4712edd0a5e642bd88cdd560d94af9
Nigless <nigless.gaming@gmail.com>
2020-10-24 19:41:01 +0300

    добавил компонент Product

---
+++ src/components/Product.tsx
@@ -0,0 +1,29 @@
+﻿import React from "react";
+import styled from "styled-components"
+
+
+interface Props {
+	className?: string,
+	img: string
+	title: string
+}
+
+const Title = styled.div({
+	fontWeight: 600,
+	textAlign: 'center'
+})
+
+const Img = styled.img({
+	height: 200
+})
+
+export default function Container({ className, img, title }: Props) {
+
+	return (
+		<div>
+			<Img src={img} alt={title} />
+			<Title>{title}</Title>
+		</div>
+
+	)
+}

ef1d4873238057c82bba67dd3a660e14487c3681
Nigless <nigless.gaming@gmail.com>
2020-10-24 19:07:01 +0300

    добавил обзац

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import laptopImg from './laptop.png';
 import cubesImg from './cubes.png';
+import ratingImg from './rating.png';
 import Navigation from '../Navigation/Navigation';
 import Footer from '../Footer/Footer';
 import Item from '../Grid/Item'
 import Container from '../Container';
 import Grid from '../Grid/Grid';
 import Break from '../Grid/Break';
+import Flex from '../Flex'
 
 const Header = styled(Grid)({
 	height: '100vh',
@@ -35,40 +37,56 @@ export default function App(): JSX.Element {
 		<>
 			<Navigation />
 			<Container maxWidth="1200px">
-				<Header align="center" wrap="header">
-					<Item>
-						<Title>
-							Made different
+				<Flex align="center">
+					<Header wrap="header">
+						<Item>
+							<Title>
+								Made different
 						</Title>
-					</Item>
-				</Header>
+						</Item>
+					</Header>
+				</Flex>
 			</Container>
 			<Body maxWidth="1200px">
-				<Grid align="center">
-					<Item width="1fr">
-						<Promo src={laptopImg} />
-					</Item>
-					<Item width="2fr">
-						<section>
-							<h2>Lorem ipsum dolor sit amet</h2>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+				<Flex align="center">
+					<Grid>
+						<Item width="1fr">
+							<Promo src={laptopImg} />
+						</Item>
+						<Item width="2fr">
+							<section>
+								<h2>Lorem ipsum dolor sit amet</h2>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
 							</p>
-						</section>
-					</Item>
-					<Break />
-					<Item width="2fr">
-						<section>
-							<h2>Lorem ipsum dolor sit amet</h2>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+							</section>
+						</Item>
+						<Break />
+						<Item width="2fr">
+							<section>
+								<h2>Lorem ipsum dolor sit amet</h2>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
 							</p>
-						</section>
-					</Item>
-					<Item width="1fr">
-						<Promo src={cubesImg} />
-					</Item>
-				</Grid>
+							</section>
+						</Item>
+						<Item width="1fr">
+							<Promo src={cubesImg} />
+						</Item>
+						<Break />
+						<Item width="1fr">
+							<Promo src={ratingImg} />
+						</Item>
+						<Item width="2fr">
+							<section>
+								<h2>Lorem ipsum dolor sit amet</h2>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+								</p>
+							</section>
+						</Item>
+					</Grid>
+				</Flex>
 			</Body>
 			<Footer />
 		</>
